bc045d285560dd0a686bca744a3d0ee9a227af54
lmavridis <lazaros.mavridis@made.com>
2017-03-13 17:57:04 +0000

    documentation

--- R/weightingFunctions.R
+++ R/weightingFunctions.R
@@ -1,7 +1,11 @@
-
-abjustSimMatrix <- function(data, weights) {
+#' A function that applies a weighting factor to the columns of a table
+#'
+#' @param data a table with similarity scores
+#' @param weights a vector with weights for the columns of the data table
+#' @param default a default weight to be applied
+abjustSimMatrix <- function(data, weights, default = 0) {
   weights.init <-  data.table(sku = colnames(data),
-                              weights = 0)
+                              weights = default)
   weights.given <- data.table(sku = names(weights),
                               weights = weights)
   weights.final <- merge(weights.init, weights.given, by = "sku", all.x=TRUE)
